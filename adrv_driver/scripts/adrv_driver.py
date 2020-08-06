b23c5c566e335abfd423702a5603fdce4b6d82d6
DaiGuard <zenith.or.w@gmail.com>
2020-08-07 00:24:50 +0900

    add package 'adrv_driver'

---
+++ adrv_driver/scripts/adrv_driver.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+import math
+
+from pwm_com import PWMCom
+
+
+steer_vel = 0.0
+drive_vel = 0.0
+
+
+def recvTargetVel(data):
+
+  global drive_vel
+  global steer_vel
+
+  drive_vel = data.linear.x
+  steer_vel = data.angular.z
+
+
+def linear_set(data, lower, upper, offset):
+
+  val = 0.0
+
+  data = data + offset
+
+  if data > 0.0:    
+    val = abs(data / (upper + offset))
+
+    if val > 1.0:
+      val = 1.0
+
+  else:
+    val = - abs(data / (lower+offset))
+
+    if val < -1.0:
+      val = -1.0
+
+  return val
+
+
+def execute():
+
+  # Initialize rospy
+  rospy.init_node('adrv_driver', anonymous=True)
+
+  # Frame rate
+  loop = rospy.Rate(10)
+
+  # Paramaters
+  steer_ch = 0
+  drive_ch = 1
+  steer_max = math.pi / 2.0
+  steer_min = - math.pi / 2.0
+  steer_offset = 0.0
+  drive_max = 1.0
+  drive_min = -1.0
+  drive_offset = 0.0
+
+  try:
+    steer_ch = rospy.get_param('~steer_ch')
+    drive_ch = rospy.get_param('~drive_ch')
+  except rospy.ROSException:
+    pass
+
+  # Subscribe
+  rospy.Subscriber('cmd_vel', Twist, recvTargetVel)
+
+  # PWM Communicator initialize
+  com = PWMCom(1, 50.0)
+  ret, message = com.InitPWM(steer_ch, 0.001, 0.002)
+  if not ret:
+    raise Exception(message)
+
+  ret, message = com.InitPWM(drive_ch, 0.001, 0.002)
+  if not ret:
+    raise Exception(message)
+
+  # Loop
+  while not rospy.is_shutdown():
+
+    # Runtime parameter update
+    try:
+      steer_max = rospy.get_param('~steer_max')
+      steer_min = rospy.get_param('~steer_min')
+      steer_offset = rospy.get_param('~steer_offset')
+      drive_max = rospy.get_param('~drive_max')
+      drive_min = rospy.get_param('~drive_min')
+      drive_offset = rospy.get_param('~drive_offset')
+
+    except rospy.ROSException:
+      pass
+    
+    ret, message = com.SetPWM(steer_ch, linear_set(steer_vel, steer_min, steer_max, steer_offset))
+    if not ret:
+      rospy.logwarn(message)
+
+    ret, message = com.SetPWM(drive_ch, linear_set(drive_vel, drive_min, drive_max, drive_offset))
+    if not ret:
+      rospy.logwarn(message)
+
+    loop.sleep()
+
+
+if __name__ == '__main__':
+  try:
+    execute()
+  except rospy.ROSInterruptException as ex:
+    rospy.logerr(ex.message)
+  except Exception as ex:
+    rospy.logerr(ex.message)
+    
---
+++ adrv_driver/scripts/pwm_com.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+
+import Adafruit_PCA9685
+
+
+class PWMCom:
+
+  def __init__(self, port=0, hz=50.0):
+    """
+    Initialize PWMCom class
+
+    Args:
+      port: int
+        i2c port number
+      hz: float
+        pwm frequency
+    """
+
+    #----------------------#
+    # Initialize member values
+    self._port = port
+    self._hz = hz
+    self._ch_params = {}
+
+    #----------------------#
+    # Initialize PWM setting
+    self._pwm = Adafruit_PCA9685.PCA9685()    
+    self._pwm.set_pwm_freq(self._hz)
+
+
+  def __del__(self):
+    pass
+  
+  def InitPWM(self, ch, min, max):
+    """
+    Initialize PWM Output
+
+    Args:
+      ch : int
+        initialize pwm channel
+      min : float
+        minimum pwm on time (ms)
+      max : float
+        maximum pwm on time (ms)
+        
+    Returns:
+      bool:
+        result of initialize pwm task.
+        True: success
+        False: faulse
+      str:
+        result message  
+    """
+
+    bmin = int(min / ((1.0 /self._hz) / 4096))
+    bmax = int(max / ((1.0 /self._hz) / 4096))
+
+    #----------------------#
+    # value error check
+    if bmin < 0 or bmin > 4096:      
+      return False, 'Overflow minimu pwm on time'
+    if bmax < 0 or bmax > 4096:
+      return False, 'Overflow maximum pwm on time'
+    if bmin > bmax:
+      return False, 'Minimum value is greater than maximum value'    
+    if ch < 0:
+      return False, 'Out of range channel value'    
+
+    self._ch_params[ch] = [bmin, bmax]
+    self._pwm.set_pwm(ch, 0, (bmin+bmax)/2)
+
+    print '-----------------'
+    print 'CH: %d Initialize' % ch
+    print '  min: %d, max: %d' % (bmin, bmax)
+
+    return True, ""
+
+
+  def SetPWM(self, ch, ratio):
+    """
+    """
+
+    if ratio < -1.0:
+      return False, "Out of range ratio"
+    if ratio > 1.0:
+      return False, "Out of range ratio"
+    if ch < 0:
+      return False, 'Out of range channel value'
+
+    [bmin, bmax] = self._ch_params[ch]
+
+    pulse = int((1.0 + ratio) * (bmax + bmin) / 2.0)
+
+    self._pwm.set_pwm(ch, 0, pulse)
+
+    return True, ""
