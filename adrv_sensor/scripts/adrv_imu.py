77ca7c6079b489cca6f5dfb1e82f775c1fffc739
DaiGuard <zenith.or.w@gmail.com>
2020-08-07 12:18:48 +0900

    add package adrv_sensro

---
+++ adrv_sensor/scripts/adrv_imu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+
+
+def execute():
+  pass
+
+
+if __name__ == '__main__':
+
+    try:
+      execute()
+    except rospy.ROSInterruptException:
+      pass
+
